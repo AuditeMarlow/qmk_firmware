7619ef6abd2bc62bd9cfd4fadc537fe77b364fe7
Niek den Breeje <n.denbreeje@guardian360.nl>
2023-03-26 21:33:11 +0200

    feat: add personal RS40 keymap

---
+++ keyboards/kj_modify/rs40/keymaps/auditemarlow/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Audite Marlow (@auditemarlow)
+// SPDX-License-Identifier: GPL-3.0
+
+#pragma once
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM_PER_KEY
+#define RETRO_TAPPING
