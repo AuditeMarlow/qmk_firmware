1db5afcc7eded30c950ae3ea5821df4fb72eb2d8
Niek den Breeje <n.denbreeje@guardian360.nl>
2024-02-21 17:10:23 +0100

    feat: add tap dances to gaming layer

--- keyboards/splitkb/aurora/corne/keymaps/auditemarlow/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/auditemarlow/keymap.c
@@ -13,6 +13,20 @@ enum layers {
     U_FUN
 };
 
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+enum {
+	G_1,
+	G_2,
+	G_3,
+	G_4,
+	G_5
+};
+
 int base = U_BASE;
 
 #define L_BASE DF(U_BASE)
@@ -134,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [U_GAME] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     L_BASE,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,  _______,
-        KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,
+        KC_LSFT, TD(G_1), TD(G_2), TD(G_3), TD(G_4), TD(G_5),                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  _______,
                                             KC_LCTL, KC_SPC,  KC_C,             KC_ENT,  KC_BSPC, KC_DEL
     ),
 
@@ -255,7 +269,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	tap_dance_action_t *action;
+
 	switch (keycode) {
+		// Layer keys
 		case L_BASE:
 			if (record->event.pressed) {
 				base = U_BASE;
@@ -276,9 +293,60 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				base = U_GAME;
 			}
 			return true;
+
+		// Tap dance keys
+		case TD(G_1):
+		case TD(G_2):
+		case TD(G_3):
+		case TD(G_4):
+		case TD(G_5):
+			action = &tap_dance_actions[TD_INDEX(keycode)];
+			if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
 	}
+
 	return true;
 }
+
+void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
+    }
+}
+
+void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
+    }
+}
+
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
+    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
+
+tap_dance_action_t tap_dance_actions[] = {
+	[G_1] = ACTION_TAP_DANCE_TAP_HOLD(KC_1, KC_Z),
+	[G_2] = ACTION_TAP_DANCE_TAP_HOLD(KC_2, KC_X),
+	[G_3] = ACTION_TAP_DANCE_TAP_HOLD(KC_3, KC_C),
+	[G_4] = ACTION_TAP_DANCE_TAP_HOLD(KC_4, KC_V),
+	[G_5] = ACTION_TAP_DANCE_TAP_HOLD(KC_5, KC_B),
+};
 
 #ifdef OLED_ENABLE
 static void render_layer_state(void) {
